944fb73a729c39312efc78d2397a25be0b841c3a
Ben Snell <benjaminpaulsnell@gmail.com>
2018-03-18 20:22:57 -0600

    Completed S10.02

--- S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -15,21 +15,41 @@
  */
 package com.example.android.sunshine.sync;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.annotation.NonNull;
+
+import com.example.android.sunshine.data.WeatherContract;
+import com.example.android.sunshine.data.WeatherProvider;
 
 
 public class SunshineSyncUtils {
 
-//  TODO (1) Declare a private static boolean field called sInitialized
+    //  DONE (1) Declare a private static boolean field called sInitialized
+    private static boolean sInitialized;
 
-    //  TODO (2) Create a synchronized public static void method called initialize
-    //  TODO (3) Only execute this method body if sInitialized is false
-    //  TODO (4) If the method body is executed, set sInitialized to true
-    //  TODO (5) Check to see if our weather ContentProvider is empty
-        //  TODO (6) If it is empty or we have a null Cursor, sync the weather now!
-
+    //  DONE (2) Create a synchronized public static void method called initialize
+    synchronized public static void initialize(@NonNull final Context context) {
+    //  DONE (3) Only execute this method body if sInitialized is false
+        if(sInitialized == true){return;}
+    //  DONE (4) If the method body is executed, set sInitialized to true
+        sInitialized = true;
+    //  DONE (5) Check to see if our weather ContentProvider is empty
+        ContentResolver weatherResolver = context.getContentResolver();
+        Cursor weatherCursor = weatherResolver.query(WeatherContract.WeatherEntry.CONTENT_URI,
+                null,
+                null,
+                null,
+                null);
+    //  DONE (6) If it is empty or we have a null Cursor, sync the weather now!
+        if(null == weatherCursor){
+            SunshineSyncUtils.startImmediateSync(context);
+        }else if(weatherCursor.getCount() == 0){
+            SunshineSyncUtils.startImmediateSync(context);
+        }
+    }
     /**
      * Helper method to perform a sync immediately using an IntentService for asynchronous
      * execution.
